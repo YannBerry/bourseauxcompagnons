e4c93a6464318573e5bc14cdbdb9ea96d3683911
YannBerry <berry.yann@free.fr>
2019-07-04 16:57:39 +0200

    homepage update

--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
 from django.views.generic import TemplateView
 
 from activities.models import Activity
+from profiles.models import Profile
+from outings.models import Outing
 
 
 class HomepageView(TemplateView):
@@ -9,4 +11,6 @@ class HomepageView(TemplateView):
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
         context['all_activities'] = Activity.objects.all()
+        context['5_last_profiles'] = Profile.objects.order_by('-user_id')[:5]
+        context['5_last_outings'] = Outing.objects.order_by('-id')[:5]
         return context
