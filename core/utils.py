31d067b8793b685341ebe90edb26acf6bf39a35c
YannBerry <berry.yann@free.fr>
2019-10-09 21:28:42 +0200

    debbug outings calendar display

--- core/utils.py
+++ core/utils.py
@@ -1,20 +1,19 @@
-# from calendar import LocaleHTMLCalendar
-from calendar import HTMLCalendar
+from calendar import LocaleHTMLCalendar
+# from calendar import HTMLCalendar
 
 from outings.models import Outing
 
-# class Cal(LocaleHTMLCalendar):
-#     def __init__(self, firstweekday=0, locale=None, year=None, month=None):
-#         self.year = year
-#         self.month = month
-#         LocaleHTMLCalendar.__init__(self, firstweekday, locale)
-#         # super().__init__()
-
-class Cal(HTMLCalendar):
-    def __init__(self, year=None, month=None):
+class Cal(LocaleHTMLCalendar):
+    def __init__(self, firstweekday=0, locale=None, year=None, month=None):
         self.year = year
         self.month = month
-        super(Cal, self).__init__()
+        super().__init__(firstweekday=0, locale=None)
+
+# class Cal(HTMLCalendar):
+#     def __init__(self, year=None, month=None):
+#         self.year = year
+#         self.month = month
+#         super(Cal, self).__init__()
 
 class CalOutings(Cal):
     def formatday(self, day, outings):
