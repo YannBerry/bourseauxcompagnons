93c1dbb74aad9992a63462c82d5591f6fdb95cb9
YannBerry <berry.yann@free.fr>
2019-11-04 00:06:54 +0100

    index template improvments

--- core/views.py
+++ core/views.py
@@ -1,6 +1,5 @@
 from django.views.generic import TemplateView
 from datetime import date
-from django.db.models import Q
 
 from activities.models import Activity
 from profiles.models import Profile
@@ -13,7 +12,7 @@ class HomepageView(TemplateView):
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
         context['all_activities'] = Activity.objects.all()
-        context['profiles'] = Profile.objects.select_related('user').prefetch_related('activities').filter(public_profile='True')
-        context['5_last_profiles'] = Profile.objects.select_related('user').prefetch_related('activities').order_by('-user_id')[:6]
-        context['5_last_outings'] = Outing.objects.prefetch_related('activities').filter(start_date__gte=date.today()).order_by('-id')[:6]
+        context['profiles'] = Profile.objects.select_related('user').prefetch_related('activities').filter(public_profile='True', location__isnull=False)
+        context['5_last_profiles'] = Profile.objects.select_related('user').prefetch_related('activities').order_by('-user_id')[:5]
+        context['5_last_outings'] = Outing.objects.prefetch_related('activities').filter(start_date__gte=date.today()).order_by('-id')[:5]
         return context
