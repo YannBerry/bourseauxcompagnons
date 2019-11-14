8b1c8e8579d450a59d6b5195e59094e9752248fd
YannBerry <berry.yann@free.fr>
2019-11-14 21:54:47 +0100

    fixed bug in the ordering of grades per activities

--- activities/models.py
+++ activities/models.py
@@ -17,12 +17,12 @@ class Activity(models.Model):
 class Grade(models.Model):
     activity = models.ForeignKey(Activity, on_delete=models.CASCADE, verbose_name=_('activity'))
     name = models.CharField(_('name'), max_length=50)
-    # ordering = models.PositiveIntegerField(_('ordering'))
+    ordering = models.PositiveIntegerField(_('ordering'))
     
     class Meta:
         verbose_name = _('grade')
         verbose_name_plural = _('grades')
-        ordering = ['activity__name']
+        ordering = ['activity__name', 'ordering']
 
     def __str__(self):
         return self.name
