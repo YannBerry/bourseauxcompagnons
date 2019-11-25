7bafb97730e9d9a4cf53876e7ba9c655452070a2
YannBerry <berry.yann@free.fr>
2019-11-25 22:22:23 +0100

    added search outing on homepage

--- core/views.py
+++ core/views.py
@@ -1,6 +1,5 @@
 from django.views.generic import TemplateView
 from datetime import date
-from django.urls import reverse_lazy
 
 from activities.models import Activity
 from profiles.models import Profile
@@ -16,6 +15,4 @@ class HomepageView(TemplateView):
         context['profiles'] = Profile.objects.select_related('user').prefetch_related('activities').filter(public_profile='True', location__isnull=False)
         context['5_last_profiles'] = Profile.objects.select_related('user').prefetch_related('activities').order_by('-user_id')[:5]
         context['5_last_outings'] = Outing.objects.prefetch_related('activities').filter(start_date__gte=date.today()).order_by('-id')[:5]
-        context['profiles_search_url'] = reverse_lazy('profiles:search')
-        context['outings_search_url'] = reverse_lazy('outings:search')
         return context
