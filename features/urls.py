2ab924a95c46a2e0d1c70c990470c54d564b030d
yann <berry.yann@free.fr>
2020-03-21 14:05:48 +0100

    profile completion

--- features/urls.py
+++ features/urls.py
@@ -5,7 +5,4 @@ from django.views.generic.base import TemplateView
 app_name = 'features'
 urlpatterns = [
     path(_('votes/'), TemplateView.as_view(template_name="features/votes.html"), name='votes'),
-    path(_('hoortus/'), TemplateView.as_view(template_name="features/hoortus.html"), name='hoortus'),
-    path(_('hoortus/account'), TemplateView.as_view(template_name="features/account.html"), name='account'),
-    path(_('hoortus/product'), TemplateView.as_view(template_name="features/product.html"), name='product'),
 ]
