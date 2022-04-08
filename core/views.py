a6c57fad1757ae237f9d67b03602fadbee8d86d7
yann <berryyann.fr@gmail.com>
2022-04-08 17:23:27 +0200

    improving reset password mail

--- core/views.py
+++ core/views.py
@@ -37,7 +37,7 @@ class ProfilePasswordChangeView(SuccessMessageMixin, PasswordChangeView):
 
 class ProfilePasswordResetView(SuccessMessageMixin, PasswordResetView):
     form_class = ProfilePasswordResetForm
-    subject_template_name = _("Password reset")
+    subject_template_name = 'registration/password_reset_subject.html'
     success_url = reverse_lazy('login')
     success_message = _("Password reset in progress. You've received an e-mail with a link to choose your new password. If you haven't received any e-mail, please check that you have filled the form out with the e-mail address you used to register and check your spam.")
 
