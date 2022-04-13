25b5c0c12ec9adbf84fde13066089236715a75f1
yann <berryyann.fr@gmail.com>
2022-04-13 13:27:00 +0200

    simplified the no-touch display of activities with the hover

--- static/js/detect-no-touch.js
+++
@@ -1,7 +0,0 @@
-var selectableItemsListElts = document.querySelectorAll(".selectable-items-list");
-
-if (!("ontouchstart" in selectableItemsListElts)) {
-    selectableItemsListElts.forEach(function(elts) {
-      elts.classList.add("no-touch");
-    });
-}
